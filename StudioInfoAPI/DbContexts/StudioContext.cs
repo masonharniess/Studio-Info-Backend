d8df49f38ecc89f4cd0cfe96f48c089d38b56acc
Mason Harniess <maseharniess@outlook.com>
2023-08-23 14:22:07 +0100

    add DbContext to services collection, and set up project to use SQLite

--- StudioInfoAPI/DbContexts/StudioContext.cs
+++ StudioInfoAPI/DbContexts/StudioContext.cs
@@ -6,6 +6,7 @@ namespace StudioInfoAPI.DbContexts
   // The database context is the main class that coordinates Entity Framework functionality for a data model.
   public class StudioContext : DbContext
   {
+    // The options object provides configuration information for how the context should be set up. The : base(options) part calls the constructor of the base DbContext class, passing in the provided options.
     public StudioContext(DbContextOptions<StudioContext> options) : base(options) { }
 
     // DbSet is a class that represents a collection of entities from a specific table or entity type in a database. It serves as an interface to query, insert, update, and delete records in the corresponding database table.
--- StudioInfoAPI/Program.cs
+++ StudioInfoAPI/Program.cs
@@ -13,8 +13,13 @@ namespace StudioInfoAPI
       // Add services to the container.
 
       builder.Services.AddControllers().AddNewtonsoftJson();
-      builder.Services.AddDbContext<StudioContext>(opt => opt.UseInMemoryDatabase("StudioList"));
-      // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+      //builder.Services.AddDbContext<StudioContext>(opt => opt.UseInMemoryDatabase("StudioList"));
+      
+      // set up set up dependency injection for StudioContext using EF Core to connect to an SQLite database.
+      // adding DBContext to the services collection means we do not need to use the 'OnConfiguring' method in DbContext class to configure the options again.
+      builder.Services.AddDbContext<StudioContext>(DbContextOptions => DbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:StudioInfoApiDatabase"]));
+
       builder.Services.AddEndpointsApiExplorer();
       builder.Services.AddSwaggerGen();
 
